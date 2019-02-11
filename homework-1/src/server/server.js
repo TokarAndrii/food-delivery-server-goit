0b20cb96e31478b8e0691e6b705d5253c67daca7
tokarandrii <tokar.andrii@gmail.com>
2019-02-11 17:16:55 +0200

    homework-2 done find products by id

--- homework-1/src/server/server.js
+++ homework-1/src/server/server.js
@@ -1,19 +1,38 @@
 const http = require("http");
 const url = require("url");
 const morgan = require("morgan");
+const UrlPattern = require('url-pattern');
 
 const defaultRouter = require("../routes/defaultRouter");
 const productsRouter = require("../routes/productsRouter");
+const productsRouterId = require("../routes/productsRouterId");
 const signupRouter = require("../routes/signupRouter");
 
 const startServer = port => {
   const logger = morgan("combined");
   const server = http.createServer((request, response) => {
+
     const parsedUrl = url.parse(request.url);
 
+    var pattern = new UrlPattern('/products(/:id)');
+
+    const matching = pattern.match(parsedUrl.pathname)
+
+    let id;
+
+    if (matching) {
+      id = matching.id
+      request.id = id;
+    }
+
     switch (parsedUrl.pathname) {
-      case "/products":
+
+      case ("/products/"):
         logger(request, response, () => productsRouter(request, response));
+        break;
+
+      case (`/products/${id}`):
+        logger(request, response, () => productsRouterId(request, response));
         break;
 
       case "/signup":

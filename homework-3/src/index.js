55a74ba5557d8e5b5eba34ae34bd8c5c7962c840
tokarandrii <tokar.andrii@gmail.com>
2019-02-19 09:33:17 +0200

    POST /orders/  completed

--- homework-3/src/index.js
+++ homework-3/src/index.js
@@ -10,8 +10,9 @@ const router = require('./routes/router');
 // http://localhost:8083/products/19112890 - GET product by id with no found results
 // http://localhost:8083/products/?ids=19112831, 19112832,  19112833 - GET product bu query ids
 // http://localhost:8083/products/?ids=19112890, 19112899 - GET  product bu query ids with no found results
-// http://localhost:8083/users/ - POST create new user {"name": "Andrii-1", "phone": "09709709797","email": "email@com", "age": 20}
+// http://localhost:8083/users/ - POST create new user data: {"name": "Andrii-1", "phone": "09709709797","email": "email@com", "age": 20}
 // http://localhost:8083/users/ - GET all users
+// http://localhost:8083/orders/ - POST  create order.json at folder users/${UserName}/orders/ data: {"user": 1550492087349,"products": [19112831,19112832],"deliveryType": "deliveryType","deliveryType": "deliveryType",}
 
 
 
--- homework-3/src/routes/orders/ordersRouter.js
+++ homework-3/src/routes/orders/ordersRouter.js
@@ -8,10 +8,8 @@ const ordersRouter = (req, res) => {
     const newId = Date.now();
     const { user, products, deliveryType, deliveryAdress } = req.body;
     const order = { user, products, deliveryType, deliveryAdress, id: newId }
-    console.log(order, ' order');
 
     const foundUser = users.users.find(current => current.id === parseInt(user));
-    console.log(foundUser, ' - foundUser');
 
     const { name } = foundUser;
 
@@ -27,20 +25,25 @@ const ordersRouter = (req, res) => {
         return found;
     })
 
-    console.log(findAllProducts, ' - findAllProducts')
-    console.log(findAllProducts.includes(undefined), ' - findAllProducts')
-
     if (foundUser && !findAllProducts.includes(undefined)) {
 
         fs.mkdirSync(path.join(__dirname, "../../../db/users/", `${name}`), (err) => {
             if (err) throw err;
-
             console.log(`folder ${name} created`)
         });
+
+        fs.writeFile(path.join(__dirname, `../../../db/users/${name}/`, 'orders.json'), JSON.stringify(order),
+            (err) => {
+                if (err) throw err;
+                console.log("The order has been saved!");
+            })
+
 
         res.json({ "status": "success", "order": order });
         res.end();
         return;
+
+
     }
 
     res.json({ "status": "failed", "order": null });
